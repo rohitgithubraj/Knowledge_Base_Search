232bf588b3ab99e6165160b31e49518eb2ae8b41
Raj verma <rohitrajverma6202@gmail.com>
2025-06-18 20:02:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, request, jsonify, send_from_directory
 import sqlite3
+import os
 
 app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/")
 def serve_index():
-    return send_from_directory('.', 'index.html')
+    return send_from_directory(BASE_DIR, "index.html")
 
 @app.route("/<path:path>")
 def static_files(path):
-    return send_from_directory('.', path)
- 
+    return send_from_directory(BASE_DIR, path)
+
 @app.route("/search")
 def search():
     query = request.args.get("query", "")
@@ -22,5 +24,4 @@ def search():
     return jsonify(results)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
-
+    app.run(host="0.0.0.0", port=10000)
